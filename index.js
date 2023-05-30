582dd260f966042b8c75ce05468933f0bbadf4d1
lebrancconvas <lebrancconvas@gmail.com>
2023-05-30 19:16:58 +0700

    ✨ feat: edit task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Hello World!');
 
-// @todo #1 Create add() function that get two numbers and return the sum of them in number.
-function add(a, b) {
+// @todo #1 Create create() function. 
+function create() {
 
 }
